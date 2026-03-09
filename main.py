7c381ee80b37e6879abd64456e3862f77f082cb8
noahsealy <noah.sealy8@gmail.com>
2026-03-08 21:02:34 -0300

    regex solution for email extractor based on resume logic

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import sys
 from src.parsers.pdf_parser import PdfParser
 from src.parsers.word_parser import WordParser
+from src.extractors.email_extractor import EmailExtractor
 
 def main():
     print('hhello')
     pdf = PdfParser()
-    # print(pdf.parse('NoahSealyResume.pdf'))
-
     word = WordParser()
-    print(word.parse('NoahSealyResume.docx'))
+
+    resume = pdf.parse('NoahSealyResume.pdf')
+
+    email = EmailExtractor()
+    print(email.extract(resume))
 
 if __name__ == '__main__':
     sys.exit(main())
--- src/extractors/email_extractor.py
+++ src/extractors/email_extractor.py
@@ -1,10 +1,14 @@
 from src.extractors.field_extractor import FieldExtractor
+import re
 
 class EmailExtractor(FieldExtractor):
     def __init__(self):
         super().__init__('email')
 
-    # might be best for non-llm solution, as email is better to capture with rule based or ner...
     def extract(self, text: str) -> str:
+        return self.find_emails(text)[0]
+    
+    def find_emails(self, text: str) -> list[str]:
         print(text)
-        return 'email'
+        pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+        return re.findall(pattern, text)
